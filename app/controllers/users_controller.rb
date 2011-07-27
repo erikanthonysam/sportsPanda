893c6db1a1bcb3836a5c26c7e51bd601bdbce040
Erik Sam <erikanthonysam@gmail.com>
2011-07-26 21:35:22 -0700

    redirect new user to profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      redirect_to root_path
+      redirect_to @user
     else
       @title = "Sign up"
       render 'new'
